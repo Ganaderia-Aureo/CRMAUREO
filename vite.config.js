b61cf7f5fdcc4473fc2bc8ebeaeeb3dbdb148225
Ganaderia Aureo <ganaderia.aureo@gmail.com>
2026-03-15 16:10:58 +0100

    Actualización: configuración PWA, iconos y dependencias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png', 'apple-touch-icon.png', 'icons/*.png'],
+      manifest: {
+        name: 'CRM Ganadería Áureo',
+        short_name: 'Áureo',
+        description: 'Sistema de gestión para Ganadería Áureo',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#b8860b',
+        lang: 'es',
+        icons: [
+          { src: '/icons/icon-72.png', sizes: '72x72', type: 'image/png' },
+          { src: '/icons/icon-96.png', sizes: '96x96', type: 'image/png' },
+          { src: '/icons/icon-128.png', sizes: '128x128', type: 'image/png' },
+          { src: '/icons/icon-144.png', sizes: '144x144', type: 'image/png' },
+          { src: '/icons/icon-152.png', sizes: '152x152', type: 'image/png' },
+          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-384.png', sizes: '384x384', type: 'image/png' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png' },
+          { src: '/icons/icon-maskable.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}']
+      }
+    })
+  ],
 })
